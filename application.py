be371886dfd9be8011feb10df9d5927e4073bb49
kevin <khynes57@outlook.com>
2020-06-10 17:09:07 -0400

    First commit.

---
+++ application.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from config import Config
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+socketio = SocketIO(app)
+
+
+@app.route("/")
+def index():
+    return render_template("layout.html")
---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "super_secret_key"
