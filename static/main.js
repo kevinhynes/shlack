3aa0e2ce4e73e687d9720dabc5d0323f8e39ef74
kevin <khynes57@outlook.com>
2020-07-10 21:02:01 -0400

    Added some JS media query to handle sizing of messages

--- static/main.js
+++ static/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
-    ensureLogIn()
+    // ensureLogIn()
     console.log('DOM fully loaded and parsed')
+
+
+    // Resizing Messages
+    const mql = window.matchMedia('(max-width: 767px)')
+    resizeMessages(mql)
+    mql.addEventListener('change', resizeMessages)
 })
 
 
@@ -23,14 +29,31 @@ function ensureLogIn() {
 }
 
 
-$('#myCollapsible').on('hidden.bs.collapse', function () {
-  $()
-})
+
+function resizeMessages(mql) {
+    console.log('media query received')
+
+    const navbar = document.getElementById('navbar')
+    const main = document.getElementById('main')
+    const messageBox = document.getElementById('message-box')
+    const main_height = messageBox.offsetTop - navbar.offsetHeight
+    const main_top_pos = navbar.offsetTop + navbar.clientHeight
+    console.log(main_height + 'px')
+    main.style.height = main_height + 'px'
+    // main.style.top = main_top_pos + 'px'
+
+    // main.style.height = '400px';
+    console.log(`navbar.offsetTop: ${navbar.offsetTop}, navbar.clientHeight: ${navbar.clientHeight}`)
+    console.log(`main.offsetTop: ${main.offsetTop}, main.clientHeight: ${main.clientHeight}`)
+    console.log(`messageBox.offsetTop: ${messageBox.offsetTop}, messageBox.clientHeight: ${messageBox.clientHeight}`)
+
+}
+
+
+
 
 
 // document.addEventListener('DOMContentLoaded', (event) => {
 //     document.getElementById('exampleModal').modal('show');
 //     console.log('DOM fully loaded and parsed');
 // });
-
-
