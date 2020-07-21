e63366b8966d241dc668a58cfc3d06ac21a53455
kevin <khynes57@outlook.com>
2020-07-21 11:34:41 -0400

    Improvements to avatar selection

--- static/signup.js
+++ static/signup.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
-
     document.querySelectorAll(".avatar-img").forEach(avatar => {
         avatar.onclick = changeSelectedAvatar
-        console.log("signup.js function mapping")
     })
-    console.log("signup.js document.ready")
 })
 
+const userblue = "#cce5ff"
+const userblueborder = "#004085"
+const avatar_bg = "#8b878b"
+const avatar_border = "#000000"
 
 function changeSelectedAvatar() {
     const selected = document.getElementById("selected-avatar")
+    document.querySelectorAll(".avatar-img").forEach(avatar => {
+        if (avatar.src === selected.src) {
+            avatar.style.background = avatar_bg
+            avatar.style.border_color = avatar_border
+            console.log("matched avatar")
+        }
+    })
+
     selected.src = this.src
-    console.log("changeSelectedAvatar")
+    this.style.background = userblue
+    this.style.border_color = userblueborder
 }
